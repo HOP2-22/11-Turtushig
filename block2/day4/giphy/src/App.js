4c6712e64de2abd56103fcd36fa48298d0e48811
E-Turtushig <Turtushig102@gmail.com>
2022-11-23 20:05:15 +0800

    day

--- block2/day4/giphy/src/App.js
+++ block2/day4/giphy/src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { Container, Box, Typography, Button } from "@mui/material";
+import { Container, Box, Typography, Button, Grid } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
+import axios from "axios";
 import "./App.css";
 
 const App = () => {
-  const [search, setSearch] = useState();
-  const [result, setResult] = useState();
+  const [input, setInput] = useState("");
+  const [results, setResults] = useState([]);
+  const search = async () => {
+    const res = await axios.get(
+      `https://api.giphy.com/v1/gifs/search?api_key=NyWkIs62b2otHWoqEXqFQ7txM4BRZ1cU&q=${input}&limit=25&offset=0&rating=g&lang=en `
+    );
+    setResults(res.data.data);
+  };
 
   return (
     <div className="wrapper">
-      <Box className="wrapper">
-        <Container>
+      <Container>
+        <Box className="wrapper">
           <Box
             sx={{
               display: "flex",
@@ -27,15 +34,26 @@ const App = () => {
                 width: "988px",
                 height: "52px",
               }}
-              type={Text}
               placeholder={"Search All The Gifs And Stickers"}
-            ></input>
-            <Button>
+              value={input}
+              onChange={(e) => {
+                setInput(e.target.value);
+              }}
+            />
+            <Button onClick={search}>
               <SearchIcon />
             </Button>
           </Box>
-        </Container>
-      </Box>
+        </Box>
+        {results.map((result) => {
+          console.log(results);
+          return (
+            <Grid>
+              <img src={result.images.fixed_height.url} alt="" />
+            </Grid>
+          );
+        })}
+      </Container>
     </div>
   );
 };
