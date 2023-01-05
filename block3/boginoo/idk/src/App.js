90f38e2659bfd6dcf00a5a349d24ccf86b4d196c
E-Turtushig <Turtushig102@gmail.com>
2023-01-05 15:55:47 +0800

    boginoo

---
+++ block3/boginoo/idk/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="top">
+        <a href="#lmao">Хэрхэн ажиллада вэ?</a>
+        <button className="login">Нэвтрэх</button>
+      </div>
+      <div className="main">
+        <div className="logo">Boginoo</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
